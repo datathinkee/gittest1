e668752bf2ec10830de005d77fb3fe6a279570a8
Data Thinker <datathinker@outlook.com>
2019-07-12 15:52:09 -0400

    Update pandas_utility.py

--- pandas_utility.py
+++ pandas_utility.py
@@ -1,6 +1,11 @@
-# Rename columns
-# Build a list of labels: list_labels
-list_labels = ['year','artist','song','chart weeks']
+##Load CSV files
+df1 = pd.read_csv('world_population.csv')
+new_labels = ['year','population']
+df2 = pd.read_csv('world_population.csv', header=0, names=new_labels)
+df2 = pd.read_csv(file_messy, delimiter=' ', header=3, comment='#')
+df2.to_csv(file_clean, index=False)
 
-# Assign the list of labels to the columns attribute: df.columns
+
+## Rename columns
+list_labels = ['year','artist','song','chart weeks']
 df.columns = list_labels
